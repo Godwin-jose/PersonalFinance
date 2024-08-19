67669594ebd5c3bb02664e2eb6bf93977f3828fd
Godwin Jose <145746268+Godwin-jose@users.noreply.github.com>
2024-08-19 21:55:28 +0530

    Update axiosConfig.js

--- Frontend/src/axiosConfig.js
+++ Frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your API base URL
+  baseURL: 'https://personalfinance-go7i.onrender.com', // Replace with your API base URL
   
 });
 
